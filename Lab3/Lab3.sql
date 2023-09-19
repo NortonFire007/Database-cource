cda866fc228a4dae6a51f873dea10872b493ec28
Norotn <didokalexis@gmail.com>
2023-09-19 09:51:06 +0300

    Lab3 complete

---
+++ Lab3/Lab3.sql
@@ -0,0 +1,22 @@
+SELECT name
+FROM cities
+WHERE name LIKE '%ськ%';
+
+SELECT name
+FROM cities
+WHERE name LIKE '%донец%';
+
+SELECT CONCAT(name, ' (', region, ')') AS city_region
+FROM cities
+WHERE population > 100000
+ORDER BY city_region ASC;
+
+SELECT name, (population / 40000000 * 100) AS population_percentage
+FROM cities
+ORDER BY population DESC
+LIMIT 50;
+
+SELECT CONCAT(name, ' - ', (population / 40000000 * 100), '%') AS population_percentage
+FROM cities
+WHERE population / 40000000 * 100 >= 0.1
+ORDER BY population DESC;
