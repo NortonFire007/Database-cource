74cc990ed6683619d4f2880e0d68e29db07c3344
Norotn <didokalexis@gmail.com>
2023-10-30 21:48:19 +0200

    Complete lab6

---
+++ Lab6/lab6_part1.sql
@@ -0,0 +1,9 @@
+SELECT c.name AS city_name, r.name AS region_name
+FROM cities c
+JOIN regions r ON c.region = r.uuid
+WHERE c.population > 350000;
+
+SELECT c.name AS city_name
+FROM cities c
+JOIN regions r ON c.region = r.uuid
+WHERE r.name = "Nord"
---
+++ Lab6/lab6_part2.sql
@@ -0,0 +1,45 @@
+DROP TABLE IF EXISTS `Lines`
+CREATE TABLE `Lines` (
+    `line_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `line_name` VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS `Stations`;
+CREATE TABLE `Stations` (
+    `station_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `city_name` VARCHAR(255),
+    `station_name` VARCHAR(255),
+    `line_id` INT,
+    FOREIGN KEY (`line_id`) REFERENCES `Lines`(`line_id`)
+);
+
+DROP TABLE IF EXISTS `Connections`;
+CREATE TABLE `Connections` (
+    `connection_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `station_id_1` INT,
+    `station_id_2` INT,
+    `distance` DECIMAL(10, 2),
+    `travel_time` INT,
+    `connection_type` VARCHAR(255),
+    FOREIGN KEY (`station_id_1`) REFERENCES `Stations`(`station_id`),
+    FOREIGN KEY (`station_id_2`) REFERENCES `Stations`(`station_id`)
+);
+
+INSERT INTO `Lines` (`line_name`) VALUES
+    ('U1'),
+    ('U2'),
+    ('U3');
+
+INSERT INTO `Stations` (`city_name`, `station_name`, `line_id`) VALUES
+    ('Berlin', 'Alexanderplatz', 1),
+    ('Berlin', 'Zoologischer Garten', 2),
+    ('Berlin', 'Potsdamer Platz', 2),
+    ('Berlin', 'Wittenbergplatz', 1),
+    ('Berlin', 'Friedrichstraße', 3);
+
+INSERT INTO `Connections` (`station_id_1`, `station_id_2`, `distance`, `travel_time`, `connection_type`)
+VALUES
+    (1, 2, 2.5, 5, 'Direct'),
+    (2, 3, 1.8, 3, 'Direct'),
+    (3, 4, 1.2, 2, 'Direct'),
+    (4, 5, 2.0, 4, 'Direct');
